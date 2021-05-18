332707283f06e3d53fd5d80d4554dc7875bfdaf1
Bray-Ro <44115084+Bray-Ro@users.noreply.github.com>
2021-05-18 15:01:06 -0400

    Create color.h

---
+++ src/color.h
@@ -0,0 +1,37 @@
+void print_in_black() {
+        printf("\033[0;30m");
+}
+void print_in_red() {
+
+        printf("\033[0;31m");
+}
+void print_in_green() {
+
+        printf("\033[0;32m");
+}
+void print_in_yellow() {
+
+        printf("\033[0;33m");
+}
+void print_in_blue() {
+
+        printf("\033[0;34m");
+}
+void print_in_purple() {
+
+        printf("\033[0;35m");
+}
+void print_in_cyan() {
+
+        printf("\033[0;36m");
+}
+void print_in_white() {
+
+        printf("\033[0;37m");
+}
+
+
+void RESET_COLOR() {
+
+        printf("\033[0m");
+}
